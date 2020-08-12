eb294dbe61bef06d536bac70406bdf748dfd4cfe
Yugal <yugalnagarkoti1702@gmail.com>
2020-08-12 12:20:44 +0530

    Created Activities and enabled toolbar navigation icon

--- app/src/main/java/com/example/chattapp/LoginActivity.kt
+++ app/src/main/java/com/example/chattapp/LoginActivity.kt
@@ -4,17 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class RegisterActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     lateinit var toolbar: androidx.appcompat.widget.Toolbar;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
-        setSupportActionBar(findViewById(R.id.toolbar_reg))
-        toolbar= findViewById(R.id.toolbar_reg)
-        supportActionBar!!.title="Register"
+        setContentView(R.layout.activity_login)
+        setSupportActionBar(findViewById(R.id.toolbar_log))
+        toolbar= findViewById(R.id.toolbar_log)
+        supportActionBar!!.title="Login"
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         toolbar.setNavigationOnClickListener{
-            val intent= Intent(this@RegisterActivity,WelcomeActivity::class.java)
+            val intent= Intent(this@LoginActivity,WelcomeActivity::class.java)
             startActivity(intent)
             finish()
         }
